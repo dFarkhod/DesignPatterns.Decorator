6326f0854e18db6267a6d8dea78e5cd7275d7f45
Farkhod <dfarkhod@gmail.com>
2022-10-23 17:08:50 +0800

    updates

--- src/Program.cs
+++ src/Program.cs
@@ -7,14 +7,16 @@ order.Products = new List<Product>
         new Product {Name = "Tablet", Price = 100},
         new Product {Name = "Laptop", Price = 100}
     };
+Console.WriteLine($"Buyurtmaning jami narxi: {order.CalculateTotalPrice()}, oddiy buyurtma.");
 
-Console.WriteLine($"Buyurtmaning jami narxi: {order.CalculateTotalPrice()}.");
+var preOrder = new PreOrderDecorator(order);
+Console.WriteLine($"Buyurtmaning jami narxi: {preOrder.CalculateTotalPrice()}, avvaldan berilgan buyurtma.");
 
 var memberOrder = new MemberOrderDecorator(order);
-Console.WriteLine($"Buyurtmaning jami narxi: {memberOrder.CalculateTotalPrice()}, avvaldan berilgan buyurtma.");
+Console.WriteLine($"Buyurtmaning jami narxi: {memberOrder.CalculateTotalPrice()}, do'kon a'zosi bergan buyurtma.");
 
-var memberPreOrder = new PreOrderDecorator(memberOrder);
-Console.WriteLine($"Buyurtmaning jami narxi: {memberPreOrder.CalculateTotalPrice()}, do'kon a'zosining avvaldan berilgan buyurtmasi.");
+var memberPreOrder = new PreOrderDecorator(memberOrder); 
+Console.WriteLine($"Buyurtmaning jami narxi: {memberPreOrder.CalculateTotalPrice()}, do'kon a'zosining avvaldan berilgan buyurtmasi."); // successive discount
 Console.WriteLine();
 
 Console.Read();
